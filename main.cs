55d85d63b7d1b017eee68cf2c8074e408be8d80e
niccolochi09 <31803682-niccolochi09@users.noreply.replit.com>
2024-02-03 02:52:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "Niccolo";
+    string lastName = "Abella";
+    int age = 20;
+    Console.WriteLine ($"Hello world {firstName} {lastName} Age: {age}");
+    Console.WriteLine ("Hello World" +  firstName + " " +  lastName + " " + age);
+  }
+}
